1664434550540880be0322cc4039aa0a84fe3dc6
Pablo Agudelo <pablo2394@hotmail.com>
2018-07-29 23:25:02 -0500

    First classes definition

--- src/main/scala/co/com/delivery/entities/Entities.scala
+++ src/main/scala/co/com/delivery/entities/Entities.scala
@@ -9,3 +9,19 @@ sealed trait Accion
 case class MovementI(order: Order) extends Accion
 case class MovementA(order: Order) extends Accion
 case class MovementD(order: Order) extends Accion
+
+sealed trait Orientation
+case class North(s:String) extends Orientation
+case class South(s:String) extends Orientation
+case class East(s:String) extends Orientation
+case class West(s:String) extends Orientation
+
+sealed trait Horizon
+case class OrientationN(orientation: Orientation) extends Horizon
+case class OrientationS(orientation: Orientation) extends Horizon
+case class OrientationE(orientation: Orientation) extends Horizon
+case class OrientationW(orientation: Orientation) extends Horizon
+
+case class Coord(intX: Int,intY: Int)
+
+case class Position(coord: Coord,horizon: Horizon) 
--- src/main/scala/co/com/delivery/services/DeliveryService.scala
+++ src/main/scala/co/com/delivery/services/DeliveryService.scala
@@ -1,5 +1,10 @@
 package co.com.delivery.services
 
+import scala.concurrent.Future
+import co.com.delivery.entities._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+// Algebra
 class DeliveryService {
 
 }
